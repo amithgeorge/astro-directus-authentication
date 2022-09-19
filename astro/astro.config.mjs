42a88716897ddb4150a05c7e7e0d7c6808cd25fd
Amith George <strider.am@gmail.com>
2022-09-19 13:15:13 +0530

    enable SSR mode for astro

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,6 +5,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [vue()],
+	output: "server",
 	vite: {
 		ssr: {
 			noExternal: ["modern-normalize"],
