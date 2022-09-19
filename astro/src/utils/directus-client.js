42138fff3bdfea629208913c7d96ea1353c24df2
Amith George <strider.am@gmail.com>
2022-09-19 17:38:00 +0530

    reorder code in directus client to push down less important functions

--- astro/src/utils/directus-client.js
+++ astro/src/utils/directus-client.js
@@ -1,22 +1,19 @@
 import { Directus, MemoryStorage } from "@directus/sdk";
 
-export const getDirectusAdminClient = async () => {
-	const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL);
-
-	if (directus.auth.token) return directus;
-
-	if (import.meta.env.DIRECTUS_EMAIL && import.meta.env.DIRECTUS_PASSWORD) {
-		const authInfo = await directus.auth.login({
-			email: import.meta.env.DIRECTUS_EMAIL,
-			password: import.meta.env.DIRECTUS_PASSWORD,
-		});
-		// console.dir(authInfo, { depth: 5 });
-	} else if (import.meta.env.DIRECTUS_STATIC_TOKEN) {
-		await directus.auth.static(import.meta.env.DIRECTUS_STATIC_TOKEN);
+export async function getDirectusUserClient(authInfo) {
+	const storage = new MemoryStorage();
+	if (authInfo) {
+		storage.auth_token = authInfo.access_token;
+		storage.auth_refresh_token = authInfo.refresh_token;
+		storage.auth_expires = authInfo.expires;
+		storage.auth_expires_at = authInfo.expires_at;
 	}
 
+	const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL, {
+		storage: storage,
+	});
 	return directus;
-};
+}
 
 export async function getDirectusDefaultClient() {
 	const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL);
@@ -48,17 +45,20 @@ export function getStoredAuthInfo(directusClient) {
 	return authInfo;
 }
 
-export async function getDirectusUserClient(authInfo) {
-	const storage = new MemoryStorage();
-	if (authInfo) {
-		storage.auth_token = authInfo.access_token;
-		storage.auth_refresh_token = authInfo.refresh_token;
-		storage.auth_expires = authInfo.expires;
-		storage.auth_expires_at = authInfo.expires_at;
+export async function getDirectusAdminClient() {
+	const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL);
+
+	if (directus.auth.token) return directus;
+
+	if (import.meta.env.DIRECTUS_EMAIL && import.meta.env.DIRECTUS_PASSWORD) {
+		const authInfo = await directus.auth.login({
+			email: import.meta.env.DIRECTUS_EMAIL,
+			password: import.meta.env.DIRECTUS_PASSWORD,
+		});
+		// console.dir(authInfo, { depth: 5 });
+	} else if (import.meta.env.DIRECTUS_STATIC_TOKEN) {
+		await directus.auth.static(import.meta.env.DIRECTUS_STATIC_TOKEN);
 	}
 
-	const directus = new Directus(import.meta.env.PUBLIC_DIRECTUS_URL, {
-		storage: storage,
-	});
 	return directus;
 }
