43258b58fe0e528d27bb840383078a93d52c3a56
Amith George <strider.am@gmail.com>
2022-09-19 13:43:07 +0530

    read write user directus auth token to cookie

--- astro/src/pages/login.js
+++ astro/src/pages/login.js
@@ -1,4 +1,5 @@
 import { getDirectusDefaultClient } from "$src/utils/get-directus-client.js";
+import cookie from "cookie";
 
 export async function post({ request, params }) {
 	const formData = await request.formData();
@@ -10,13 +11,21 @@ export async function post({ request, params }) {
 		email: data.email,
 		password: data.password,
 	});
-	// console.dir(authInfo, { depth: 5 });
+	authInfo.expires_at = new Date().valueOf() + authInfo.expires;
+	console.dir(authInfo, { depth: 5 });
 
-	// const home = new URL("/", request.url);
-	// const response = Response.redirect(url, 302);
-	// response.headers.append("Set-Cookie" )
-
-	return new Response(JSON.stringify("Hola"), {
-		status: 200,
+	const cookieStr = cookie.serialize("auth_session", JSON.stringify(authInfo), {
+		httpOnly: true,
+		secure: import.meta.env.NODE_ENV === "production",
+		expires: new Date(authInfo.expires_at),
 	});
+
+	const home = new URL("/", request.url);
+	const response = Response.redirect(home, 302);
+	response.headers.append("Set-Cookie", cookieStr);
+
+	return response;
+	// return new Response(JSON.stringify("Hola"), {
+	// 	status: 200,
+	// });
 }
